c504301d0e87cc7734c62a02b9624717c83eef82
Pramod-munnoli <pramodmunnoli99@gmail.com>
2026-06-10 15:19:20 +0530

    feat: add user table migration

---
+++ src/migrations/1780932340000-CreateUserTable.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateUserTable1780932340000 implements MigrationInterface {
+  name = 'CreateUserTable1780932340000';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TYPE "public"."user_role_enum" AS ENUM('DOCTOR', 'PATIENT')`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "user" (
+        "id" SERIAL NOT NULL,
+        "email" character varying NOT NULL,
+        "password" character varying NOT NULL,
+        "role" "public"."user_role_enum" NOT NULL,
+        "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
+        CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"),
+        CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id")
+      )`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "user"`);
+    await queryRunner.query(`DROP TYPE "public"."user_role_enum"`);
+  }
+}
